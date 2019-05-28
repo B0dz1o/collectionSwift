3d1733b49906366e6bff1ff4e3f47fa0d00f3d7a
Piotr Bogdan <bogdzio@o2.pl>
2019-05-29 01:37:28 +0200

    Rename search view controller

---
+++ GHCollection/Controllers/SearchRepos/SearchReposController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GHCollection
+//
+//  Created by Piotr Bogdan on 28/05/2019.
+//  Copyright © 2019 Piotr Bogdan. All rights reserved.
+//
+
+import UIKit
+
+final class SearchReposController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
