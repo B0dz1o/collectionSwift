13435609a1b8edf16d812e8c34f50a1cc23eb9a1
Piotr Bogdan <bogdzio@o2.pl>
2019-05-29 05:43:03 +0200

    Add navigation controller

---
+++ GHCollection/Controllers/Navigation/SearchNavigationController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchNavigationController.swift
+//  GHCollection
+//
+//  Created by Piotr Bogdan on 29/05/2019.
+//  Copyright © 2019 Piotr Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class SearchNavigationController: UINavigationController {
+
+    init() {
+        super.init(rootViewController: SearchReposController())
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
