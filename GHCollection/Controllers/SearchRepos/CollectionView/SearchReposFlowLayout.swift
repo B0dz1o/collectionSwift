84575058434d82de6aa0db76c4084007cc87d12a
Piotr Bogdan <bogdzio@o2.pl>
2019-05-29 02:55:33 +0200

    Include collectionview-connected classes

---
+++ GHCollection/Controllers/SearchRepos/CollectionView/SearchReposFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  SearchReposFlowLayout.swift
+//  GHCollection
+//
+//  Created by Piotr Bogdan on 29/05/2019.
+//  Copyright © 2019 Piotr Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class SearchReposFlowLayout: NSObject {
+
+}
